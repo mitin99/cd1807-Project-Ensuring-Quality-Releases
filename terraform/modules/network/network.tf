9044b24be1522092c29b8c24c646867825121a5e
mitin99 <nhumatkhau@gmail.com>
2024-04-17 23:40:51 +0700

    change azurerm_subnet from test to main

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -4,7 +4,7 @@ resource "azurerm_virtual_network" "test" {
   location             = "${var.location}"
   resource_group_name  = "${var.resource_group}"
 }
-resource "azurerm_subnet" "test" {
+resource "azurerm_subnet" "main" {
   name                 = "${var.application_type}-${var.resource_type}-sub"
   resource_group_name  = "${var.resource_group}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
