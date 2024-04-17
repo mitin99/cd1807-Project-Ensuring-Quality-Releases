0bee28f9e5fb5bb18c4e659f42165c76d0b0bcc5
mitin99 <nhumatkhau@gmail.com>
2024-04-17 14:03:43 +0700

    add resource name to vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_interface" "" {
+resource "azurerm_network_interface" "main" {
   name                = "${var.application_type}-network-interface"
   location            = "var.location"
   resource_group_name = "var.resource_group"
@@ -11,7 +11,7 @@ resource "azurerm_network_interface" "" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
+resource "azurerm_linux_virtual_machine" "main" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "var.location"
   resource_group_name = "var.resource_group"
