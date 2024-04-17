e063a379ce5515e80bf90e859c3e289ba1d2923b
mitin99 <nhumatkhau@gmail.com>
2024-04-17 22:54:46 +0700

    remove loganalitic variable from vm/input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -6,5 +6,3 @@ variable "application_type" {}
 variable "resource_type" {}
 variable "public_ip_address_id" {}
 variable "subnet_id" {}
-variable "log_analytics_workspace_id" {}
-variable "log_analytics_primary_shared_key" {}
