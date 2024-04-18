ea7672963a61dad78cb1f6232ef772e0967f6b53
mitin99 <nhumatkhau@gmail.com>
2024-04-18 23:09:27 +0700

    change to vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,20 +1,20 @@
 resource "azurerm_network_interface" "main" {
   name                = "${var.application_type}-network-interface"
-  location            = "var.location"
-  resource_group_name = "var.resource_group"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "var.subnet_id"
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "var.public_ip_address_id"
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
   name                = "${var.application_type}-${var.resource_type}"
-  location            = "var.location"
-  resource_group_name = "var.resource_group"
+  location            = var.location
+  resource_group_name = var.resource_group
   size                = "Standard_DS2_v2"
   admin_username      = "adminuser"
   network_interface_ids = [azurerm_network_interface.main.id]
@@ -41,16 +41,4 @@ resource "azurerm_virtual_machine_extension" "main" {
   type                       = "OmsAgentForLinux"
   type_handler_version       = "1.0"
   auto_upgrade_minor_version = true
-  
-  settings = <<SETTINGS
-    {
-        "workspaceId": "${var.log_analytics_workspace_id}"
-    }
-SETTINGS
-
-  protected_settings = <<PROTECTEDSETTINGS
-    {
-        "workspaceKey": "${var.log_analytics_primary_shared_key}"
-    }
-PROTECTEDSETTINGS
 }
